9528d08b3991973d60edc7a8ffb8990d4d130836
Dylan <dquesada186@gmail.com>
2018-04-17 14:17:04 -0500

    Added Routes MVC and DBContext.

---
+++ RubbishBrigade/Models/Pickup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.Entity;
+namespace RubbishBrigade.Models
+{
+    public class Pickup
+    {
+        public int ID { get; set; }
+        public string Address { get; set; }
+        public DateTime PickupDate { get; set; }
+        public decimal Price { get; set; }
+    }
+    public class PickupDBContext : DbContext
+    {
+        public DbSet<Pickup> Pickups { get; set; }
+    }
+}
